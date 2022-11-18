eaebcd77f15a9b25f1bd16553bd40512027364dd
Quentin Rouchet <quentin.rouchet@outlook.com>
2022-11-18 14:43:46 +0100

    Build chapter 2

--- src/components/list.js
+++ src/components/list.js
@@ -2,48 +2,31 @@ import { useState } from "react";
 import "../sass/components/list.scss";
 
 function Checkbox() {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleChange = event => {
+      if (event.target.checked) {
+        console.log('✅ Checkbox is checked');
+      } else {
+        console.log('⛔️ Checkbox is NOT checked');
+      }
+      setIsChecked(current => !current);
+    };
+
     return(
-        <input type="checkbox"></input>
+        <input type="checkbox" value={isChecked} onChange={handleChange}></input>
     );
 }
-
-// export default function List() {
-//     return (
-//         <div className="section-li">
-//             <h2>Todos</h2>
-//             <ul>
-//                 <li><Checkbox /> Learn React</li>
-//                 <li><Checkbox /> Be Awesome!</li>
-//             </ul>
-//         </div>
-//     );
-// }
-
-
-// export default function List() {
-//     const initialTodos = ["Salut mon pote React", "My second todo"];
-//     const [todos, setTodos] = useState(initialTodos);
-//     return (
-//         <div className="section-li">
-//             <h2>Todos</h2>
-//             <ul>
-//                 {todos.map((todo) => (
-//                     <li>
-//                         <Checkbox /> {todo}
-//                     </li>
-//                 ))}
-//             </ul>
-//         </div>
-//     )
-// }
 
 export default function List() {
 
     //state
     const [todos, setTodos] = useState([
-        {id: 1, nom:"Learn React"}
-        {id: 2, nom:"My second todo"}
+        {id: 1, task:"Salut React"},
+        {id: 2, task:"My second todo"}
     ]);
+
+    //comportement
 
     //render
     return (
@@ -51,7 +34,9 @@ export default function List() {
             <h2>Todos</h2>
             <ul>
                 {todos.map((todo) => {
-                    return
+                    return <li>
+                        <Checkbox />{todo.task}
+                    </li>
                 })}
             </ul>
         </div>
