914209b13256e1523cae58bb2bd00ca17234bcfd
Quentin Rouchet <quentin.rouchet@outlook.com>
2022-11-21 13:55:11 +0100

    update form part to look more like the example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import Input from './components/input.js';
 import Title from './components/title.js';
-import Button from './components/button.js';
+import Form from './components/form.js'
 import List from './components/list.js';
 
 /* sass import */
@@ -11,10 +10,7 @@ export default function App() {
     <>
       <Title />
       <section className="container">
-        <div className='add-todo'>
-          <Input />
-          <Button />
-        </div>
+        <Form />
         <List />
       </section>
     </>
--- src/components/input.js
+++
@@ -1,7 +0,0 @@
-import "../sass/components/input.scss"
-
-export default function Input () {
-    return (
-        <input className="add-task" placeholder="Type a new todo"></input>
-    )
-}
